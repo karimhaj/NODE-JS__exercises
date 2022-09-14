85d2cde8b4699fd8204826f6f56ad344e1e15a29
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-09-14 19:06:19 +0200

    configure jest with typescript and add an API route

--- dist/server.js
+++ dist/server.js
@@ -3,13 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-require("express-async-errors");
-const app = (0, express_1.default)();
-app.get("/", (request, response) => {
-    response.send('Hello everyone');
-});
+const app_1 = __importDefault(require("./app"));
 const port = 3000;
-app.listen(port, () => {
+app_1.default.listen(port, () => {
     console.log(`[server]: server is running at http//:localhost:${port}`);
 });
