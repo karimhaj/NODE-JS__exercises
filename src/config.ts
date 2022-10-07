b1350fc7bbca51ef3732e1f2a12e3a0e609d5e5d
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-10-07 20:23:20 +0200

    configure github authentication strategy

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,11 @@
+const missingSetting = "Warning! No Value set for this environment variable";
+
 const config = {
     "PORT": process.env.PORT || 3000,
-    "SESSION_SECRET": process.env.SESSION_SECRET || "missing setting"
+    "SESSION_SECRET": process.env.SESSION_SECRET || missingSetting,
+    "GITHUB_CLIENT_ID": process.env.GITHUB_CLIENT_ID || missingSetting,
+    "GITHUB_CLIENT_SECRET": process.env.GITHUB_CLIENT_SECRET || missingSetting,
+    "GITHUB_CALLBACK_URL": process.env.GITHUB_CALLBACK_URL || missingSetting,
 }
 
 export default config; 
